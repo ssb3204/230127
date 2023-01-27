b506969b332efc6b403d98312456c782978a236e
ssb3204 <ryanp3@naver.com>
2023-01-27 17:59:27 +0900

    B10171

---
+++ src/BaekP/B10171.java
@@ -0,0 +1,14 @@
+package BaekP;
+
+public class B10171 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");//역슬레쉬 \\를\로 읽는다
+		System.out.println(" )  ( ')"); 
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
