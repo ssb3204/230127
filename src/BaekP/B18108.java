4a3d51b44dd2a278c2bb09a6fb09e2606f0606eb
ssb3204 <ryanp3@naver.com>
2023-01-27 18:06:16 +0900

    B18108

---
+++ src/BaekP/B18108.java
@@ -0,0 +1,13 @@
+package BaekP;
+import java.util.*;
+
+public class B18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt()-543;
+		System.out.println(a);
+	}
+
+}
