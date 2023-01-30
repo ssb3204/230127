6added29b9be55a1df779996fc662057e29d9f07
ssb3204 <ryanp3@naver.com>
2023-01-30 18:27:10 +0900

    for문 내에서 +1 for문 밖에서 +1 시키는 차이점?

---
+++ src/Tutoring/B2566.java
@@ -0,0 +1,33 @@
+package Tutoring;
+import java.util.*;
+import java.lang.*;
+
+public class B2566 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		
+		int [][]arr= new int[9][9];
+		int big=arr[0][0];
+		
+		int x=0;
+		int y=0;
+		
+		for(int i=0;i<9;i++) {
+			for(int j=0;j<9;j++) {
+				arr[i][j]=scan.nextInt();
+				if(big<arr[i][j]) {
+					big=arr[i][j]; 
+					x=i+1;
+					y=j+1;
+				}
+			}
+		}
+		scan.close();
+		
+		System.out.println(big);
+		System.out.println(x+" "+y);
+		
+	}
+}
