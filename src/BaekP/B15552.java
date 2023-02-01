836d9fae56d7dbfcbd546fd8261936caf967b584
ssb3204 <ryanp3@naver.com>
2023-02-01 19:24:42 +0900

    주석 추가

--- src/BaekP/B15552.java
+++ src/BaekP/B15552.java
@@ -16,7 +16,8 @@ public class B15552 {
 
 		for(int i=0;i<tc;i++) {
 			StringTokenizer st= new StringTokenizer(br.readLine());
-			
+			// br.readLine()으로 한줄 받아오면 공백을 구별자로 한다는 선언 필요 없나?
+			//구별 없으면 ex) 1 1을 어떻게 두 개의 정수로 보는가?
 			int a=Integer.parseInt(st.nextToken());
 			int b=Integer.parseInt(st.nextToken());
 			
