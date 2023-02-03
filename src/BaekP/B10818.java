f35d5decea7a2b2ef697fb9aadb9c53f13555ee8
ssb3204 <ryanp3@naver.com>
2023-02-03 20:28:53 +0900

    Arrays.sort()으로 오름차순 정렬후 출력

---
+++ src/BaekP/B10818.java
@@ -0,0 +1,26 @@
+package BaekP;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10818 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int tc= Integer.parseInt(br.readLine());
+		int[] arr= new int[tc];
+		StringTokenizer st= new StringTokenizer(br.readLine());
+
+		for(int i=0;i<tc;i++) {
+			arr[i]= Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[0]+" "+arr[arr.length-1]);
+		
+	}
+}
