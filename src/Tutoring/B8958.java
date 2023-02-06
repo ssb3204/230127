88fbddcb133ba0b2d0a49d0ddf7942d59028a54e
ssb3204 <ryanp3@naver.com>
2023-02-07 01:13:12 +0900

    arr[i].charAt(j) String으로 받아서 한 인덱스에 여러 문자포함가능

---
+++ src/Tutoring/B8958.java
@@ -0,0 +1,37 @@
+package Tutoring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B8958 {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc=Integer.parseInt(br.readLine());
+		String[] arr= new String [tc];
+
+		
+		for(int i=0;i<tc;i++) {
+			arr[i]=br.readLine();
+		}
+		for(int i=0;i<tc;i++) {
+			int cnt=0;
+			int sum=0;
+			for(int j=0;j<arr[i].length();j++) {
+				if(arr[i].charAt(j)=='O') {
+					cnt++;
+				}
+				else {
+					cnt=0;
+				}
+				sum+=cnt;
+			}
+			System.out.println(sum);
+		}
+		
+	}
+
+}
