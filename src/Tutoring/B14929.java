3e63985e647311293fbc1d05f3987c46a5abc14b
ssb3204 <ryanp3@naver.com>
2023-02-22 22:06:11 +0900

    변수 int long 변위 인지 확실히

---
+++ src/Tutoring/B14929.java
@@ -0,0 +1,30 @@
+package Tutoring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B14929 {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		StringTokenizer st= new StringTokenizer(br.readLine());
+	
+		int arr[]=new int[tc+1];
+		int sum[]=new int[tc+1];
+		for(int i=1;i<=tc;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+			sum[i]=sum[i-1]+arr[i];
+		}
+		int total=0;
+		
+		for(int i=1;i<=tc;i++) {
+			total+=arr[i]*(sum[tc]-sum[i]);
+		}
+		System.out.println(total);
+	}
+
+}
