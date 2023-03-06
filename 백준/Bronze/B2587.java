5687fd32f1329300e82c64436c0d941ccc9795d2
ryan(SON) <ryanp3@naver.com>
2023-03-06 23:25:09 +0900

    배열 sort 이후 중앙값 평균값

---
+++ 백준/Bronze/B2587.java
@@ -0,0 +1,24 @@
+package B230306;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2587 {
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		
+		int arr[]= new int[5];
+		int total=0;
+		for(int i=0;i<5;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+			total=total+arr[i];
+		}
+		Arrays.sort(arr);
+		System.out.println(total/5);
+		System.out.println(arr[2]);
+		
+	}
+}
