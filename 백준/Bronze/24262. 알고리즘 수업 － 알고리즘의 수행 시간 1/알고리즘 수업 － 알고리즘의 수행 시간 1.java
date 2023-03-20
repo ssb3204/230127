9ab3dc893c72366753ea06e47c4f97a273d5b2bd
ryan(SON) <ryanp3@naver.com>
2023-03-20 14:57:49 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,16 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("1");
+		System.out.println("0");
+	}
+	
+	
+}
