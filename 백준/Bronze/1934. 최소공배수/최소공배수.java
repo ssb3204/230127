38b73b38f2a1b722914716b2f79c904bccfef140
ryan(SON) <ryanp3@naver.com>
2023-03-26 00:48:22 +0900

    [Bronze I] Title: 최소공배수, Time: 144 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br =new BufferedReader (new InputStreamReader(System.in));
+		int tc=Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<tc;i++) {
+			StringTokenizer st =new StringTokenizer(br.readLine());
+			int a= Integer.parseInt(st.nextToken());
+			int b= Integer.parseInt(st.nextToken());
+			
+			int c=gcd(a,b);
+			
+			sb.append(a*b/c).append("\n");
+		}
+		System.out.println(sb);
+	}
+	public static int gcd(int a,int b) {
+		while(b!=0) {
+			int r=a%b;
+			
+			a=b;
+			b=r;
+		}
+		return a;
+	}
+
+}
