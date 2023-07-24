66f4f22b3761f1ca91c1714567e962549e56a148
ryan(SON) <ryanp3@naver.com>
2023-07-24 17:18:03 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Long a = Long.parseLong(st.nextToken());
+		Long b = Long.parseLong(st.nextToken());
+		
+		System.out.println((a*b)/2);
+		
+	}
+
+}
