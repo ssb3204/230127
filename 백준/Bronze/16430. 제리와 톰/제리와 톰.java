78d5f0a459568c14b3c8436890d302572856fc60
ryan(SON) <ryanp3@naver.com>
2024-02-03 02:09:39 +0900

    [Bronze V] Title: 제리와 톰, Time: 156 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int f = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int r =b-f;
+		
+		System.out.println(r+" "+b);
+	}
+
+}
