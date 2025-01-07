bb3851f77e8adf36de3ee4173673a0ac0af04d69
ryan(SON) <ryanp3@naver.com>
2025-01-07 21:36:31 +0900

    [Silver IV] Title: 한수, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print(cnt_check(Integer.parseInt(br.readLine())));
+		
+	}
+ 
+	public static int cnt_check(int num) {
+		int cnt = 0;
+		if (num < 100) {
+			return num;
+		}
+		else {
+			cnt = 99;
+			for (int i = 100; i <= num; i++) {
+				int hun = i / 100;
+				int ten = (i / 10) % 10;
+				int one = i % 10;
+ 
+				if ((hun - ten)==(ten - one)){
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+ 
+}
