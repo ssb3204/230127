f65962d5e032ca4019808bc3bb2a947678ee50c4
ryan(SON) <ryanp3@naver.com>
2025-01-21 15:38:46 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.51 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string.toLowerCase();
+        
+        char arr[]= answer.toCharArray();
+        
+        Arrays.sort(arr);
+        
+        return String.valueOf(arr);
+    }
+}
