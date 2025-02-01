7b73e1ae97500f2bdae342343ab5e68ccf519e8c
ryan(SON) <ryanp3@naver.com>
2025-02-01 16:21:24 +0900

    [level 0] Title: 문자열 밀기, Time: 17.34 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = -1;
+        String str=A;
+        for(int i=0;i<A.length();i++){
+            if(A.equals(B)){
+                answer=i;
+                break;
+            }
+            A= A.charAt(A.length()-1)+A.substring(0,A.length()-1);
+        }
+        
+        
+        return answer;
+    }
+}
