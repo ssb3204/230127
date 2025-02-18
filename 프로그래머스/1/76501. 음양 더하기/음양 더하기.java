2d2ca1cdc2706cee6899189705159bc8c9dd1741
ryan(SON) <ryanp3@naver.com>
2025-02-18 21:03:07 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 123456789;
+        int sum=0;
+        for(int i=0;i<signs.length;i++){
+            if(signs[i]){
+                sum+=absolutes[i];
+            }else{
+                sum-=absolutes[i];
+            }
+        }
+        return sum;
+    }
+}
